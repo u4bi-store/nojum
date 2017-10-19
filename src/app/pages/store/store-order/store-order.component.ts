cd3a7ea0a175ad76d4c84ce11a42f631876552e5
u4bi <rootcode10@gmail.com>
2017-10-19 13:44:47 +0900

    store order component prototype render

--- src/app/pages/store/store-order/store-order.component.ts
+++ src/app/pages/store/store-order/store-order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-store-order',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoreOrderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public location: Location) { }
 
   ngOnInit() {
   }
