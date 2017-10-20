8bfbf7af8eb74d6426d27b00067c60b29421d50f
u4bi <rootcode10@gmail.com>
2017-10-20 22:43:23 +0900

    component routing module render
    - create
    - reset
    - success
    
    - store

---
+++ src/app/pages/reset/reset.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ResetRoutingModule } from './reset-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ResetRoutingModule
+  ],
+  declarations: []
+})
+export class ResetModule { }
---
+++ src/app/pages/success/success.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SuccessRoutingModule } from './success-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    SuccessRoutingModule
+  ],
+  declarations: []
+})
+export class SuccessModule { }
