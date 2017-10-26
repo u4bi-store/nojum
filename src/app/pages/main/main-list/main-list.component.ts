3e81c770489bb73f7387f5ab5094a24c944ef96a
u4bi <rootcode10@gmail.com>
2017-10-26 22:43:24 +0900

    list card move observable timer render

--- src/app/pages/main/main-list/main-list.component.ts
+++ src/app/pages/main/main-list/main-list.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnDestroy, ElementRef, ViewChild } from '@angular/core';
 import { CoreService } from '../../../providers/core.service';
 
 import { DragScroll } from 'angular2-drag-scroll';
+import { Observable, Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-main-list',
   templateUrl: './main-list.component.html',
   styleUrls: ['./main-list.component.css']
 })
-export class MainListComponent implements OnInit {
+export class MainListComponent implements OnDestroy {
 
-  public leftNavDisabled = false;
-  public rightNavDisabled = false;
+  private subscription : Subscription;
+  private flagNav : boolean = false;
+  private isHover : boolean = false;
+
   public imagelist = [
     '#0055B2',
     '#00AE4C',
@@ -22,47 +25,34 @@ export class MainListComponent implements OnInit {
     '#0055B2',
     '#00AE4C',   
     '#0055B2',
-    '#00AE4C',
-    '#0055B2',
-    '#00AE4C',
-    '#0055B2',
-    '#00AE4C',
-    '#0055B2',
-    '#00AE4C',
-    '#0055B2',
-    '#00AE4C',
-    '#0055B2',
-    '#00AE4C',
-    '#0055B2',
-    '#00AE4C',
-    '#0055B2',
-    '#00AE4C',
-    '#0055B2',
-    '#00AE4C',
-    '#0055B2',
-    '#00AE4C',
-    '#0055B2',
-    '#00AE4C',
-    '#0055B2',
-    '#00AE4C',
-    '#0055B2',
-    '#00AE4C',
-    '#0055B2',
     '#00AE4C'
   ];
   @ViewChild('nav', {read: DragScroll}) ds: DragScroll;
 
-  constructor(public core : CoreService) { }
+  constructor(public core : CoreService) {
+    
+    let timer = Observable.timer(0, 500);
+    this.subscription = timer.subscribe((e => {
+      
+      if(!this.isHover) this.flagNav ? this.ds.moveLeft() : this.ds.moveRight();
 
-  ngOnInit() {
+    }));
+
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
   leftBoundStat(reachesLeftBound: boolean) {
-    this.leftNavDisabled = reachesLeftBound;
+    if(reachesLeftBound) this.flagNav = false;
   }
 
   rightBoundStat(reachesRightBound: boolean) {
-    this.rightNavDisabled = reachesRightBound;
+    if(reachesRightBound) this.flagNav = true;
+
   }
+
+  listHover = (bool : boolean) => this.isHover = bool;
 
 }
